b2e1fd8fd0e8514087b9142cad285e648e8a6d86
Sayeh Ghaderi <sayeh.ghaderi9@gmail.com>
2022-09-20 19:56:34 +0200

    Add project files.

---
+++ Calclass/Cal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calclass
+{
+    public class Cal
+    {
+       public static double  Addition(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return result;
+        }
+
+        public double  Subtraction(double  num1, double  num2)
+        {
+            double  result = num1 - num2;
+            return result;
+        }
+
+        public double  Multiplication(double  num1, double  num2)
+        {
+            double  result = num1 * num2;
+            return result;
+        }
+
+        public double  Division(double  num1, double  num2)
+        {
+            double  result = num1 / num2;
+            return result;
+        }
+
+    }
+}
---
+++ Calclass/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Calclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 ;
+            double num2;
+            int opt;
+            bool loop = true;
+           
+
+            while (loop)
+            {
+
+                Console.Clear();
+
+                Console.Write("----------------------calculator-------------------------\n");
+                Menu();
+
+                Console.Write("Enter the first Number :");
+                num1 = double.Parse(Console.ReadLine());
+                Console.Write("Enter the second Number :");
+                num2 = double.Parse(Console.ReadLine()); ;
+
+
+                opt = Convert.ToInt32(Console.ReadLine());
+              
+
+
+
+                switch (opt)
+                {
+                    case 1:
+                        
+                        cal.Addition();
+                         break;
+
+                    case 2:
+                     
+
+                        break;
+
+                    case 3:
+                      
+
+                        break;
+
+                    case 4:
+                        
+                        break;
+
+                    case 5:
+                        loop = false;
+                        break;
+
+                    default:
+                        Console.Write("Input correct option\n");
+                        break;
+
+                }
+
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey(); //without this line, the loop will work but the switch cases will not work. it is just a loop of menu without any adding and so on.
+
+                static void Menu()
+                {
+                    Console.WriteLine("----- Menu -----");
+                    Console.WriteLine("1: +");
+                    Console.WriteLine("2: -");
+                    Console.WriteLine("3: *");
+                    Console.WriteLine("4: /");
+                    Console.WriteLine("5: exit");
+
+                }
+
+
+            }
+        }
+    }
+}
